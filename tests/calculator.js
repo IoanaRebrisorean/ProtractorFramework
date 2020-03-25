7c5f60a9d2a0831808b3b6faeee4b1e7de705c82
IoanaRebrisorean <ioana.rebrisorean@yahoo.com>
2020-03-25 16:55:40 -0700

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,33 @@
+let homepage = require ('../pages/homepage')
+//facem asta ca sa avem acces la homepage page
+
+describe ('demo calculator tests', function(){
+
+    it('addition tests', function(){
+       // browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        
+
+        // element(by.model('first')).sendKeys('2');
+        homepage.enterFirstNumber('4');
+
+        // element(by.model('second')).sendKeys('4');
+        homepage.enterSecondNumber('3');
+
+        // element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+        
+        
+        // let result = element(by.cssContainingText('.ng-binding', '6'));
+        // expect(result.getText()).toEqual('6');
+        homepage.verifyResult(7);
+    
+        
+        browser.sleep(2000);
+
+    });
+
+
+
+});
